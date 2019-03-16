7b43336d37dfbf2937bb6276fd0eb9b3a8435856
Dayton <dayton@daytons-macbook-pro.local>
2019-03-15 23:32:48 -0500

    Initial Commit

---
+++ PhotosTests/PhotosTests.swift
@@ -0,0 +1,29 @@
+//
+//  PhotosTests.swift
+//  PhotosTests
+//
+//  Created by Dayton on 3/15/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import XCTest
+@testable import Photos
+
+class PhotosTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+
+}
---
+++ PhotosUITests/PhotosUITests.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosUITests.swift
+//  PhotosUITests
+//
+//  Created by Dayton on 3/15/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import XCTest
+
+class PhotosUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+    }
+
+}
